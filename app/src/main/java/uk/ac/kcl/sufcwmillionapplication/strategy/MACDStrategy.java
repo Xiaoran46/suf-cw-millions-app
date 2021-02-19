44d68a0cb6407664dc1c4df595ec7c131f8b100b
Xiaoran46 <57056248+Xiaoran46@users.noreply.github.com>
2021-02-19 15:44:53 +0000

    Update MACDStrategy.java

--- app/src/main/java/uk/ac/kcl/sufcwmillionapplication/strategy/MACDStrategy.java
+++ app/src/main/java/uk/ac/kcl/sufcwmillionapplication/strategy/MACDStrategy.java
@@ -6,9 +6,12 @@ import uk.ac.kcl.sufcwmillionapplication.bean.CalculateResult;
 import uk.ac.kcl.sufcwmillionapplication.bean.DailyQuote;
 
 public class MACDStrategy implements FinanceStrategy{
-
-    @Override
-    public List<CalculateResult> calculate(List<DailyQuote> dailyQuoteList) {
-        return null;
+    //short_term and long_term can get from the result of EMA(for example, MACD = EMA(26)-EMA(12))
+    public static double[]<CalculateResult> calculate(double[] short_term, double[] long_term) {
+        double[] result = new double[short_term.length];
+        for(int i = 0; i < short_term.length; i++) {
+            result[i] = short_term[i] - long_term[i];
+        }
+        return result;
     }
 }
