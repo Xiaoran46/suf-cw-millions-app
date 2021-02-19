cfe4eeca04997c8dc11cda6399cfa10ef2ace145
Xiaoran46 <57056248+Xiaoran46@users.noreply.github.com>
2021-02-19 16:25:40 +0000

    Update MACDStrategy.java

--- app/src/main/java/uk/ac/kcl/sufcwmillionapplication/strategy/MACDStrategy.java
+++ app/src/main/java/uk/ac/kcl/sufcwmillionapplication/strategy/MACDStrategy.java
@@ -9,7 +9,9 @@ import uk.ac.kcl.sufcwmillionapplication.strategy.EMAStrategy;
 
 public class MACDStrategy implements FinanceStrategy{
     //short_term and long_term can get from the result of EMA(for example, MACD = EMA(26)-EMA(12))
-    public static List<CalculateResult> calculate(ArrayList short_term, ArrayList long_term) {
+    public static List<CalculateResult> calculate(List<DailyQuote> dailyQuoteList) {
+        ArrayList short_term = EMAStrategy.calculate(dailyQuoteList, 12);
+        ArrayList long_term = EMAStrategy.calculate(dailyQuoteList, 26);
         ArrayList result = new ArrayList();
         for(int i = 0; i < short_term.size(); i++) {
             result.add((Double)short_term.get(i) - (Double)long_term.get(i));
